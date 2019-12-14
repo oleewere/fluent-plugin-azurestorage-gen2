1912e57fe64c4bc49153612018d0939c91061e72
Oliver Szabo <oleewere@gmail.com>
2019-12-14 17:57:40 +0100

    remove abfs client module

--- lib/fluent/plugin/out_azurestorage_gen2.rb
+++ lib/fluent/plugin/out_azurestorage_gen2.rb
@@ -1,8 +1,8 @@
 require 'net/http'
 require 'json'
+require 'tempfile'
 require 'time'
 require 'fluent/plugin/output'
-require 'fluent/plugin/abfs_client'
 
 module Fluent::Plugin
     class AzureStorageGen2Output < Fluent::Plugin::Output
@@ -27,8 +27,10 @@ module Fluent::Plugin
         config_param :auto_create_container, :bool, :default => false
         config_param :format, :string, :default => "out_file"
         config_param :command_parameter, :string, :default => nil
+        config_param :time_slice_format, :string, :default => '%Y%m%d'
 
         DEFAULT_FORMAT_TYPE = "out_file"
+        URL_DOMAIN_SUFFIX = '.dfs.core.windows.net'
 
         config_section :format do
             config_set_default :@type, DEFAULT_FORMAT_TYPE
@@ -66,7 +68,9 @@ module Fluent::Plugin
                               else
                                 'blob'
                             end
-            @configured_time_slice_format = conf['time_slice_format']
+            @azure_storage_path = ''
+            @last_azure_storage_path = ''
+            @current_index = 0
         end
 
         def multi_workers_ready?
@@ -79,8 +83,26 @@ module Fluent::Plugin
             super
         end
 
-        def write(chuck)
-            AbfsClient::upload(@azure_container, @azure_access_token)
+        def write(chunk)
+            metadata = chunk.metadata
+            tmp = Tempfile.new("azure-")
+            begin
+                chunk.write_to(tmp)
+                tmp.close
+                generate_log_name(metadata, @current_index)
+                if @last_azure_storage_path != @azure_storage_path
+                    @current_index = 0
+                    generate_log_name(metadata, @current_index)
+                end
+                content = File.open(tmp.path, 'rb') { |file| file.read }
+                #append_blob(content, metadata)
+                log.info "Metadata: #{metadata}"
+                log.info "Storage path: #{@azure_storage_path}"
+                upload(@azure_container, @azure_access_token, log)
+                @last_azure_storage_path = @azure_storage_path
+            ensure
+                tmp.unlink
+            end
         end
 
         def format(tag, time, record)
@@ -88,10 +110,26 @@ module Fluent::Plugin
             @formatter.format(tag, time, r)
         end
 
-
         private
         def ensure_container
-            AbfsClient::container_exists(@azure_container, @)
+            container_exists(@azure_container, @azure_access_token, log)
+        end
+
+        private
+        def generate_log_name(metadata, index)
+            time_slice = if metadata.timekey.nil?
+                       ''.freeze
+                     else
+                       Time.at(metadata.timekey).utc.strftime(@time_slice_format)
+                     end
+            path = @path_slicer.call(@path)
+            values_for_object_key = {
+                "%{path}" => path,
+                "%{time_slice}" => time_slice,
+                "%{index}" => index
+            }
+            storage_path = @azure_object_key_format.gsub(%r(%{[^}]+}), values_for_object_key)
+            @azure_storage_path = extract_placeholders(storage_path, metadata)
         end
 
         def setup_access_token
@@ -136,5 +174,21 @@ module Fluent::Plugin
             end
             @azure_access_token = token
         end
+
+        private
+        def container_exists
+            log.info "Check container exists"
+        end
+    
+        private
+        def create_container
+            log.info "Create container"
+        end
+    
+        private
+        def upload
+            log.info "Upload file #{@azure_access_token}"
+        end
+
     end
 end
